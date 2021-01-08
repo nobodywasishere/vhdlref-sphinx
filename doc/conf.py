834915f041dff2b515dfc1eea7b7f507dfb71161
Michael Riegert <michael@eowyn.net>
2021-01-07 23:03:26 -0500

    Missing loads import

--- doc/conf.py
+++ doc/conf.py
@@ -14,6 +14,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
 from pathlib import Path
+from json import dump, loads
 
 # -- Project information -----------------------------------------------------
 
